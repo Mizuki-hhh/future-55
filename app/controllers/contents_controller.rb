204f1764d7211733f2b3202ee3d6a8584e747ad4
Mizuki-hhh <higashishita.m@gmail.com>
2021-01-11 22:17:51 +0900

    コントローラとアソシエーション

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,4 +1,24 @@
 class ContentsController < ApplicationController
   def index
+    @contents = Content.includes(:user)
+  end
+
+  def new
+    @content = Content.new
+  end
+
+  def create
+    @content = Content.new(content_params)
+    if @content.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def content_params
+    params.require(:content).permit(:title, :image_content, :video_content, :overview, :writing, :source).merge(user_id: current_user.id)
   end
 end
