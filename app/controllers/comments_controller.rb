40a8ce81a15c1134c0b54194d167316f2d19e161
Mizuki-hhh <higashishita.m@gmail.com>
2021-01-13 17:51:47 +0900

    非同期コメント機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,14 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.new(comment_params)
+    @content = Content.find(params[:content_id])
+    @comments = @content.comments.includes(:user)
     if @comment.save
-      redirect_to content_path(@comment.content)
+      ActionCable.server.broadcast 'comment_channel', content: @comment, name: @comment.user.name, time: @comment.created_at.strftime("%Y/%m/%d %H:%M:%S"), id: @content.id
     else
-      @content = @comment.content
-      @comments = @content.comments
-      render 'contents/show'
+      # @content = @comment.content
+      # @comments = @content.comments
+      render "contents/show"
     end
   end
 
@@ -16,4 +18,5 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, content_id: params[:content_id])
   end
+
 end
