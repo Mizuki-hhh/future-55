d9f1f05187d22a20dbd9b0b22261d2714f8fac23
Mizuki-hhh <higashishita.m@gmail.com>
2021-01-13 18:18:13 +0900

    日付の表示形式編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
     @content = Content.find(params[:content_id])
     @comments = @content.comments.includes(:user)
     if @comment.save
-      ActionCable.server.broadcast 'comment_channel', content: @comment, name: @comment.user.name, time: @comment.created_at.strftime("%Y/%m/%d %H:%M:%S"), id: @content.id
+      ActionCable.server.broadcast 'comment_channel', content: @comment, name: @comment.user.name, time: @comment.created_at.strftime("%Y-%m-%d %H:%M:%S"), id: @content.id
     else
       # @content = @comment.content
       # @comments = @content.comments
