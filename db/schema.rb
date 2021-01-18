6f6d8e159c885fb7119bebb9562a3cce8ba0e415
Mizuki-hhh <higashishita.m@gmail.com>
2021-01-18 18:53:08 +0900

    マイグレーション実行＆アソシエーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_065222) do
+ActiveRecord::Schema.define(version: 2021_01_18_093526) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "ancestry"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text", null: false
@@ -32,6 +40,8 @@ ActiveRecord::Schema.define(version: 2021_01_13_065222) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_contents_on_category_id"
     t.index ["user_id"], name: "index_contents_on_user_id"
   end
 
@@ -54,5 +64,6 @@ ActiveRecord::Schema.define(version: 2021_01_13_065222) do
 
   add_foreign_key "comments", "contents"
   add_foreign_key "comments", "users"
+  add_foreign_key "contents", "categories"
   add_foreign_key "contents", "users"
 end
