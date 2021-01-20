5c4f58633afd7918a72d7c9807663108eff35de0
Mizuki-hhh <higashishita.m@gmail.com>
2021-01-20 11:34:45 +0900

    favoritesコントローラ＆ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   resources :contents do
     resources :comments, only: :create
+    post '/favorite/:content_id' => 'favorites#favorite', as: 'favorite'
+    delete '/favorite/:content_id' => 'favorites#unfavorite', as: 'unfavorite'
     collection do
       get 'get_category_children', defaults: { format: 'json' }
     end
@@ -12,6 +14,11 @@ Rails.application.routes.draw do
       get 'get_category_children', defaults: { format: 'json' }
     end
   end
+
   resources :categories, only: :show
-  resources :users, only: :show
+
+  resources :users, only: [:show] do
+    get :favorites, on: :collection
+  end
+  
 end
