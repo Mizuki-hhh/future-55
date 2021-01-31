7307039f26012d165715c38f5d4951870b614524
Mizuki-hhh <higashishita.m@gmail.com>
2021-02-01 07:21:55 +0900

    エラーの後のカテゴリー選択

--- app/javascript/category.js
+++ app/javascript/category.js
@@ -1,3 +1,47 @@
+// $(function(){
+//   function appendOption(category){
+//     var html = `<option value='${category.id}'>${category.name}</option>`;
+//     return html;
+//   }
+//   function appendChidrenBox(insertHTML){
+//     var childSelectHtml = '';
+//     childSelectHtml = `<div class='listing-select-wrapper__added' id= 'children_wrapper'>
+//                         <div class='listing-select-wrapper__box'>
+//                           <select class='listing-select-wrapper__box--select' id='child_category' name='content[category_id]'>
+//                             <option value='---' category_id='---'>---</option>
+//                             ${insertHTML}
+//                           </select>
+//                         </div>
+//                       </div>`;
+//     $('.listing-content-detail__category').append(childSelectHtml);
+//   }
+//   $('#parent_category').on('change', function() {
+//     $("#children_wrapper").remove();
+//     var parentId = document.getElementById("parent_category").value;
+//     if (parentId != '---'){
+//       $.ajax({
+//         url: 'get_category_children',
+//         type: 'GET',
+//         data: { parent_id: parentId },
+//         dataType: 'json'
+//       })
+//       .done(function(children){
+//         $("#children_wrapper").remove();
+//         var insertHTML = "";
+//         children.forEach(function(child){
+//           insertHTML += appendOption(child);
+//         });
+//         appendChidrenBox(insertHTML);
+//       })
+//       .fail(function(){
+//         alert('カテゴリー取得に失敗しました1');
+//       })
+//     }else{
+//       $("#children_wrapper").remove();
+//     }
+//   })
+// });
+
 $(function(){
   function appendOption(category){
     var html = `<option value='${category.id}'>${category.name}</option>`;
@@ -18,9 +62,9 @@ $(function(){
   $('#parent_category').on('change', function() {
     $("#children_wrapper").remove();
     var parentId = document.getElementById("parent_category").value;
-    if (parentId != '---'){
       $.ajax({
-        url: 'get_category_children',
+        url: '/contents/:id/get_category_children',
+        // url: 'get_category_children',
         type: 'GET',
         data: { parent_id: parentId },
         dataType: 'json'
@@ -34,10 +78,7 @@ $(function(){
         appendChidrenBox(insertHTML);
       })
       .fail(function(){
-        alert('カテゴリー取得に失敗しました1');
+        // alert('カテゴリー取得に失敗しました1');
       })
-    }else{
-      $("#children_wrapper").remove();
-    }
   })
 });
